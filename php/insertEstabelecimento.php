814c264b35e62880ab981b6031745e89b672ac9e
danielrt <danielrt@ifes.edu.br>
2023-12-05 15:19:20 -0300

    hahahah9

--- php/insertEstabelecimento.php
+++ php/insertEstabelecimento.php
@@ -73,7 +73,7 @@
             error_log("idFotoEstab " . $idFotoEstab, 0);
             error_log("idEnderecoEstab " . $idEnderecoEstab, 0);
 
-            $insertEstab = $db->prepare("INSERT INTO ESTABELECIMENTO (nota_media, nome, FK_endereco_endereco_PK, FK_tipo_estabelecimento_tipo_estabelecimento_PK, FK_foto_estabelecimento_foto_estabelecimento_PK, FK_selo_selo_PK) VALUES (null, '$nome_estabelecimento', $idEnderecoEstab, '$tipo_estab', $idFotoEstab, null)");
+            $insertEstab = $db->prepare("INSERT INTO ESTABELECIMENTO (nota_media, nome, FK_endereco_endereco_PK, FK_tipo_estabelecimento_tipo_estabelecimento_PK, FK_foto_estabelecimento_foto_estabelecimento_PK, FK_selo_selo_PK) VALUES (null, '$nome_estabelecimento', '$idEnderecoEstab', '$tipo_estab', '$idFotoEstab', null)");
             
 			error_log("entrou aqui 0", 0);
             $resultInsertEstab = $insertEstab->execute();
